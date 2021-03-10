2de01d1387692063edadf9731374978e2a603006
deepi <deepi7samurai@gmail.com>
2021-03-10 16:20:01 -0500

    trying to push code in github

---
+++ Application.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		//LabelTest app = new LabelTest();
+		//TextFieldTest app = new TextFieldTest();
+		//ButtonTest app = new ButtonTest();
+		//CheckBoxTest app = new CheckBoxTest();
+		//RadioButtonTest app = new RadioButtonTest();
+		//ComboxBoxTest app = new ComboxBoxTest();
+		ListTest app = new ListTest();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
